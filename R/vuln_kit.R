c4c58342584eaf35d2665aab88f5e195a351a1f9
troyhill <hill.troy@gmail.com>
2014-04-11 17:45:58 -0400

    update

--- R/vuln_kit.R
+++ R/vuln_kit.R
@@ -1,18 +1,16 @@
 ### Code to transform tidal data to a set of resources to evaluate vulnerability to sea level rise
 ### 
 ### Required arguments: 
-###     level: A numeric vector of water levels.
+###      level: A numeric vector of water levels.
 ###
-###     datetime: A POSIX* vector of time stamps that correspond to the measurements in 'level'.
-###
-###     MHW: Mean high water, in the same units specified by 'units' argument and 
-###         relative to the same vertical datum as 'level.'
+###      datetime: A POSIX* vector of time stamps that correspond to the measurements in 'level'.
 ###
 ###      platform: Elevation of the marsh platform (or another vertical position of interest). Should be
 ###         in the units specified by 'units' argument and relative to the same vertical datum as 'level.'
 ###
 ### Optional arguments: 
-###      units: 'meters' is default; 'feet' is alternative. If units are "feet," data is converted internally and output in meters.
+###      units: 'meters' is default; 'feet' is alternative. If units are "feet," data is converted internally 
+###         and output in meters.
 ###
 ###      frq.dur.inc: Elevation interval used to calculate flooding frequencies, durations, and wet/dry periods.
 ###         Defaults to 0.005 m. Units must correspond to 'units' argument.
@@ -21,7 +19,7 @@
 ###         Defaults to 0.1 m. Units must correspond to 'units' argument.
 
 
-vuln.kit <- function(level, datetime, MHW, platform = MHW, units = "meters", frq.dur.inc = 0.005, 
+vuln.kit <- function(level, datetime, platform, units = "meters", frq.dur.inc = 0.005, 
                      TV.inc = 0.1) {
   
   if(is.numeric(level) == FALSE)
@@ -33,19 +31,16 @@ vuln.kit <- function(level, datetime, MHW, platform = MHW, units = "meters", frq
   if(length(level) != length(datetime))
     stop("invalid entry: 'datetime' must be POSIXct or POSIXlt")
   
-  if(is.numeric(platform) == FALSE)
-    stop("invalid entry: 'platform' elevation must be numeric")
-  
   if(units == "feet") {
     level <- level * 0.3048
   } else if(units != "meters") {
     stop("invalid units: must be 'feet' or 'meters'")
   }
   
-  if(is.numeric(MHW) == FALSE) {
-    stop("invalid entry: 'MHW' must be a numeric input")
-  } else if(is.numeric(MHW) == TRUE & units == "feet") {
-    MHW <- MHW * 0.3048
+  if(is.numeric(platform) == FALSE) {
+    stop("invalid entry: 'platform' must be a numeric input")
+  } else if(is.numeric(platform) == TRUE & units == "feet") {
+    platform <- platform * 0.3048
   }
   
   if(is.numeric(frq.dur.inc) == FALSE)
@@ -61,35 +56,41 @@ vuln.kit <- function(level, datetime, MHW, platform = MHW, units = "meters", frq
   }
   
   t.int <- (as.numeric( datetime[2] ) - as.numeric( datetime[1] ) )  / 60
+  total.time <- t.int * length(level) / 60
+  fr.year <- total.time / 24 / 365.242    
   elevation <- seq(from = -1, to = 0.5, by = frq.dur.inc)
   
   frq.dur.df <- data.frame(elevation)
   
 
   wl <- data.frame(datetime, level)
-  wl <- wl[wl$datetime <= wl$datetime[nrow(wl)] & wl$datetime >= (wl$datetime[nrow(wl)] - 60*60*24*365), ]
-  wl$zeroed <- wl[,2] - MHW - (platform - MHW)
+  wl <- wl[wl$datetime <= wl$datetime[nrow(wl)] & wl$datetime > (wl$datetime[nrow(wl)] - 60*60*24*365), ]
+  wl$zeroed <- wl[,2] - platform
   
   for(i in 1:length(elevation)) {
     
     wl$ind <- ifelse(wl$zeroed > elevation[i] & !is.na(wl$zeroed), 1, 0)
     wl.rle <- rle(wl$ind)
     
-    nos <- c(0,cumsum(wl$ind[-1L] != wl$ind[-length(wl$ind)]))   # numbers flooding events
-    if(wl$ind[1] == 1) {   # if sequence starts out flooded, no. of flooding events is max of even numbers % 2
-      frq.dur.df$frequency[i] <- round((max(nos[nos %% 2 == 0]) / 2), 0)           # F (yr-1)
+    nos <- c(0,cumsum(wl$ind[-1L] != wl$ind[-length(wl$ind)])) 
+    if(wl$ind[1] == 1) {  
+      frq.dur.df$frequency[i] <- round((max(nos[nos %% 2 == 0]) / 2), 0)  
     } else if(wl$ind[1] == 0) {
       frq.dur.df$frequency[i] <- round((max(nos[nos %% 2 != 0]) / 2), 0)
     }
     
-    frq.dur.df$duration[i] <- max(cumsum(wl$ind) ) * t.int / 60 # D (hr/yr)
-    frq.dur.df$D90[i]  <- as.numeric(quantile(wl.rle$lengths[wl.rle$values == 1], 0.9)) * t.int / 60  # hr
-    frq.dur.df$A[i]    <- mean(wl$zeroed[wl$ind == 1], na.rm = T) - elevation[i]    # m
+    frq.dur.df$duration[i] <- max(cumsum(wl$ind) ) * t.int / 60
+    frq.dur.df$D90[i]  <- as.numeric(quantile(wl.rle$lengths[wl.rle$values == 1], 0.9)) * t.int / 60 
+    frq.dur.df$A[i]    <- mean(wl$zeroed[wl$ind == 1], na.rm = T) - elevation[i]
   }
   
   df <- frq.dur.df[frq.dur.df$elevation == 0, 2:5]
-  df$DV   <- ( frq.dur.df$duration[frq.dur.df$elevation == paste("-", TV.inc, sep="")] - frq.dur.df$duration[frq.dur.df$elevation == 0] ) / (TV.inc*100 * frq.dur.df$duration[frq.dur.df$elevation == 0]) * 100   # units: % / cm
-  df$D90V <- ( frq.dur.df$D90[frq.dur.df$elevation == paste("-", TV.inc, sep="")] - frq.dur.df$D90[frq.dur.df$elevation == 0] ) / (TV.inc*100 * frq.dur.df$D90[frq.dur.df$elevation == 0]) * 100                  # units: % / cm
+  df$DV   <- ( frq.dur.df$duration[frq.dur.df$elevation == paste("-", TV.inc, sep="")] - 
+                 frq.dur.df$duration[frq.dur.df$elevation == 0] ) / (TV.inc*100 * 
+                 frq.dur.df$duration[frq.dur.df$elevation == 0]) * 100 
+  df$D90V <- ( frq.dur.df$D90[frq.dur.df$elevation == paste("-", TV.inc, sep="")] - 
+                 frq.dur.df$D90[frq.dur.df$elevation == 0] ) / (TV.inc*100 * 
+                 frq.dur.df$D90[frq.dur.df$elevation == 0]) * 100
   rownames(df) <- NULL
   
   par(mfrow = c(2,2))
