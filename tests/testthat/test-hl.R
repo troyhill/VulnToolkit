93ee9a054490812627a24efee9f77cd4e647d653
thill <thill@ibis.sfnrc.nps.gov>
2018-11-23 13:55:18 -0500

    another test

--- tests/testthat/test-hl.R
+++ tests/testthat/test-hl.R
@@ -10,3 +10,10 @@ test_that("fld.dur gives correct output", {
   expect_equal(fld.dur(2, 1:10), 0.8)
   expect_error(fld.dur(2, "string")) 
 })
+
+
+test_that("HL gives correct output", {
+  expect_error(HL("hello world"))
+  expect_error(HL(1:10, "hello world")) 
+  expect_equal(nrow(HL(rep(c(c(1:100), c(100:1)), times = 400)[1:8761], seq.POSIXt(from = ISOdate(1910,1,1), to = ISOdate(1911,1,1), by = "hours"))), 1868)
+})
