52d1899df48f4b5db99b4ccd3965519f098057c9
thill <thill@ibis.sfnrc.nps.gov>
2018-11-23 13:58:30 -0500

    test

--- tests/testthat/test-hl.R
+++ tests/testthat/test-hl.R
@@ -3,11 +3,15 @@ library(VulnToolkit)
 
 test_that("fld.frq gives correct output", {
   expect_equal(fld.frq(2, 1:10, units = "percent"), 0.8)
-  expect_error(fld.frq(2, "string")) 
+  expect_equal(fld.frq(2, 1:10, units = "tides"), 9)
+  expect_error(fld.frq("string", 1:10)) 
+  expect_error(fld.frq(2, "string"))
+  expect_error(fld.frq(2, 1:10, units = "furlongs")) 
 })
 
 test_that("fld.dur gives correct output", {
   expect_equal(fld.dur(2, 1:10), 0.8)
+  expect_error(fld.dur("string", 1:10)) 
   expect_error(fld.dur(2, "string")) 
 })
 
